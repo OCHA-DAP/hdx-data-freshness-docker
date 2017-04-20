4bfb4c1f3dad8726ca1d103e993c12cdae795604
Michael Rans <rans@email.com>
2017-04-20 11:23:22 +0200

    need sudo

--- runapp.sh
+++ runapp.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cp /dev/null $(docker inspect -f '{{.LogPath}}' hdxdatafreshnessdocker_app_1)
+sudo cp /dev/null $(docker inspect -f '{{.LogPath}}' hdxdatafreshnessdocker_app_1)
 cd /data/homes/freshness/hdx-data-freshness-docker
 docker-compose start app
 
