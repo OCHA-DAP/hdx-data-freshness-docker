79ecf2e76e4ad1ecff03a741860ecd8f371ead52
Michael Rans <rans@email.com>
2017-04-20 10:47:51 +0200

    start app

--- runapp.sh
+++ runapp.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cp /dev/null $(docker inspect -f '{{.LogPath}}' hdxdatafreshnessdocker_app_1)
-cd /data/homes/freshness
-docker run --env-file hdx-data-freshness-docker/.env mcarans/hdx-data-freshness > cron.log 2>&1
+cd /data/homes/freshness/hdx-data-freshness-docker
+docker-compose start app
 
