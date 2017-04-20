1bb8cdfb312396e907cf51747c3af006ad7a95a6
Michael Rans <rans@email.com>
2017-04-20 11:28:56 +0200

    docker-compose path

--- runapp.sh
+++ runapp.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo cp /dev/null $(docker inspect -f '{{.LogPath}}' hdxdatafreshnessdocker_app_1)
 cd /data/homes/freshness/hdx-data-freshness-docker
-docker-compose start app
+/usr/local/bin/docker-compose start app
 
