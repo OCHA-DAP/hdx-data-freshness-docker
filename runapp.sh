5110774422ea2059e7917430492f16a7648a51cc
Michael Rans <rans@email.com>
2017-07-31 15:53:07 +0200

    Update runapp

--- runapp.sh
+++ runapp.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-sudo cp /dev/null $(docker inspect -f '{{.LogPath}}' hdxdatafreshnessdocker_app_1)
+sudo cp /dev/null $(docker inspect -f '{{.LogPath}}' hdxdatafreshnessdocker_fresh_1)
+sudo cp /dev/null $(docker inspect -f '{{.LogPath}}' hdxdatafreshnessdocker_email_1)
 cd /data/homes/freshness/hdx-data-freshness-docker
-/usr/local/bin/docker-compose start app
+/usr/local/bin/docker-compose start fresh
+/usr/local/bin/docker-compose start email
 
