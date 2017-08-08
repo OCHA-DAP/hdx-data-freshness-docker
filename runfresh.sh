c4e6048a2ad3ec316257816a08b3658378d821a5
Michael Rans <rans@email.com>
2017-08-08 10:59:30 +0200

    Separate runners

--- runfresh.sh
+++ runfresh.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 sudo cp /dev/null $(docker inspect -f '{{.LogPath}}' hdxdatafreshnessdocker_fresh_1)
-sudo cp /dev/null $(docker inspect -f '{{.LogPath}}' hdxdatafreshnessdocker_email_1)
 cd /data/homes/freshness/hdx-data-freshness-docker
 /usr/local/bin/docker-compose start fresh
-/usr/local/bin/docker-compose start email
 
