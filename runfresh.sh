07f7f2b12e7e0e6a220c781bb9801236c28e2561
Serban Teodorescu <teodorescu.serban@gmail.com>
2019-05-02 11:21:03 +0300

    update email and fresh scripts

--- runfresh.sh
+++ runfresh.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-sudo cp /dev/null $(docker inspect -f '{{.LogPath}}' hdxdatafreshnessdocker_fresh_1)
-cd /data/homes/freshness/hdx-data-freshness-docker
-/usr/local/bin/docker-compose start fresh
 
+basedir=/data/freshness
+projdir=$basedir/stack
+doco=/usr/local/bin/docker-compose
+cname=freshness_fresh_1
+
+sudo cp /dev/null $(docker inspect -f '{{.LogPath}}' $cname)
+cd $projdir
+$doco run --rm fresh
