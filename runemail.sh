15cd2fbd17812de36f7986423932c082dfa0fb4a
Serban Teodorescu <teodorescu.serban@gmail.com>
2019-05-02 11:32:47 +0300

    stop copying devnull to fresh and email containers. not needed.

--- runemail.sh
+++ runemail.sh
@@ -5,6 +5,5 @@ projdir=$basedir/stack
 doco=/usr/local/bin/docker-compose
 cname=freshness_email_1
 
-sudo cp /dev/null $(docker inspect -f '{{.LogPath}}' $cname)
 cd $projdir
 $doco run --rm email
--- runfresh.sh
+++ runfresh.sh
@@ -5,6 +5,5 @@ projdir=$basedir/stack
 doco=/usr/local/bin/docker-compose
 cname=freshness_fresh_1
 
-sudo cp /dev/null $(docker inspect -f '{{.LogPath}}' $cname)
 cd $projdir
 $doco run --rm fresh
